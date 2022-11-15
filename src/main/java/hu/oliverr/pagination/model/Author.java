aa7c2e050ccbca03b70ad9423102eb65bfabad87
Olivér <oliver.mrakovics@gmail.com>
2022-11-15 15:58:49 +0100

    added models

---
+++ src/main/java/hu/oliverr/pagination/model/Author.java
@@ -0,0 +1,37 @@
+package hu.oliverr.pagination.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "authors")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @NonNull
+    @Column(name = "first_name", length = 32)
+    private String firstName;
+
+    @NonNull
+    @Column(name = "last_name", length = 32)
+    private String lastName;
+}
---
+++ src/main/java/hu/oliverr/pagination/model/Book.java
@@ -0,0 +1,43 @@
+package hu.oliverr.pagination.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @NonNull
+    @Column(name = "title", length = 32)
+    private String title;
+
+    @NonNull
+    private String description;
+
+    @NonNull
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private Author author;
+}
